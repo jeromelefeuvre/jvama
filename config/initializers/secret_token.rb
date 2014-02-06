ced30116bb01db8b445ef224f8c0afacd698da44
Jerome Lefeuvre <jerome.lefeuvre@gmail.com>
2014-02-06 14:37:35 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jvama::Application.config.secret_key_base = '630c8ebacd96eda02a1e90e1f4b3539aee51d2d27045991572e840b27b724f3041f51bdb6a5cf4982d37de16d4851d823137cf69739ae24b892daeb6284ba174'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jvama::Application.config.session_store :cookie_store, key: '_jvama_session'
